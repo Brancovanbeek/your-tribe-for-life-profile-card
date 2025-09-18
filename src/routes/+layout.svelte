362f016b856d4be049f07f8fb0757b9ef90f6bdc
Brancovanbeek <branco.van.beek@hva.nl>
2025-09-18 12:15:35 +0200

    fix: layout.svelte error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,13 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	let { member, children } = $props();
+
 
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
-<h1 class="large">BRANCO VAN BEEK</h1>
-<!-- <h1 class="large">{member.name}</h1> -->
 
 {@render children?.()}
